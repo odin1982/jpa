13e3c90bd8f932e14726b53a195c21ed50dae8a2
odin-huawei <odin.araujo.barragan@gmail.com>
2021-07-17 22:36:33 -0500

    Los metodos hashcode y equals

--- mastering-jpa-with-hibernate/src/org/jpa/model/Customer.java
+++ mastering-jpa-with-hibernate/src/org/jpa/model/Customer.java
@@ -152,6 +152,31 @@ public class Customer {
 	private void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	@Override
 	public String toString() {
 		return "Customer [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", birthdate=" + birthdate
